c6d2d5c9aa38bcc77828dd26aa205a98e84fda10
Zhu Yang <zhuyang329@gmail.com>
2020-11-15 21:26:43 +0100

    Generates new variables. Implements rule 1: No two tents are adjacent in any of the (up to) 8 directions

--- tents_formula.py
+++ tents_formula.py
@@ -10,8 +10,8 @@ class Grid:
     def __str__(self):
         return str({'height': self.height,'width': self.width, 'trees': self.trees, 'tents in row': self.tents_in_row, 'tents in column': self.tents_in_col})
 
-# reads the grid input
 def gridInput(gridFile):
+    '''Reads the grid input.'''
     with open(gridFile) as f:
         height, width = map(int, f.readline().split())
         trees = []
@@ -21,7 +21,33 @@ def gridInput(gridFile):
             tents_in_row.append(int(tentNum))
             for (cellIndex, cell) in enumerate(line):
                 if cell == 'T':
-                    trees.append((lineIndex+1, cellIndex+1))
+                    trees.append((lineIndex, cellIndex))
         tents_in_col = [int(t) for t in f.readline().split()]
         return Grid(height, width, trees, tents_in_row, tents_in_col)
 
+clauses = [] # Stores all the clauses
+varDict = {}  # Stores all the generated variables
+
+def generateVar(varDescr):
+    '''Generates a variable from the variable description.'''
+    if varDescr not in varDict:
+        varDict[varDescr] = len(varDict) + 1
+    return varDict[varDescr]
+
+def tentVar(r, c):
+    '''Cell at (r, c) contains a tent.'''
+    return generateVar(('tent', r, c))
+
+def noTentIndex(r, c):
+    '''Neighboring cells of a cell with tent can't have tents.'''
+    return [(r_n, c_n) for r_n in [r-1, r, r+1] if r_n >= 0 for c_n in [c-1, c, c+1] if c_n >= 0 if not ((r_n == r) and (c_n == c))]
+
+grid = gridInput('tent-inputs/tents-8x8-e1.txt')
+for r1 in range(grid.height):
+    for c1 in range(grid.width):
+        for (r2, c2) in noTentIndex(r1, c1):
+            clauses.append((-tentVar(r1, c1), -tentVar(r2, c2)))
+
+# Cells with trees can't contain tents
+for (r, c) in grid.trees:
+    clauses.append((-tentVar(r,c),))
