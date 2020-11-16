1d9701696cdcb8756919085a5cf87593fc91844e
Zhu Yang <zhuyang329@gmail.com>
2020-11-16 15:01:03 +0100

    Implements: #tents <= (#tree-to-tent mappings <= )#trees

--- tents_formula.py
+++ tents_formula.py
@@ -38,11 +38,20 @@ def tentVar(r, c):
     '''Cell at (r, c) contains a tent.'''
     return generateVar(('tent', r, c))
 
+def treeVar(r, c, r_tent, c_tent):
+    '''The tree in cell (r, c) matches the tent in cell (r_tent, c_tent).'''
+    return generateVar(('tree', r, c, r_tent, c_tent))
+
+def boundFilter(lis, height, width):
+    '''Filters out coordinates that are out of bounds of a grid.'''
+    return [(x,y) for (x,y) in lis if x >=0 and x <= height-1 if y >= 0 and y <= width]
+
 def noTentIndex(r, c):
     '''Neighboring cells of a cell with tent can't have tents.'''
-    return [(r_n, c_n) for r_n in [r-1, r, r+1] if r_n >= 0 for c_n in [c-1, c, c+1] if c_n >= 0 if not ((r_n == r) and (c_n == c))]
+    return boundFilter([(r_n, c_n) for r_n in [r-1, r, r+1] for c_n in [c-1, c, c+1] if not ((r_n == r) and (c_n == c))], grid.height, grid.width)
 
 grid = gridInput('tent-inputs/tents-8x8-e1.txt')
+# No two tents are adjacent in an of the (up to) 8 directions
 for r1 in range(grid.height):
     for c1 in range(grid.width):
         for (r2, c2) in noTentIndex(r1, c1):
@@ -51,3 +60,25 @@ for r1 in range(grid.height):
 # Cells with trees can't contain tents
 for (r, c) in grid.trees:
     clauses.append((-tentVar(r,c),))
+
+def pair_clause_helper(lis):
+    '''Chooses 2 variables out of a list to build clauses.'''
+    return [(ele1, ele2) for ele1 in lis for ele2 in lis if ele2 != ele1]
+
+# Every tree matches at most one tent
+for (r, c) in grid.trees:
+    neighbor_cells = pair_clause_helper(boundFilter([(r-1, c), (r+1, c), (r, c-1), (r, c+1)], grid.height, grid.width))
+    for ((r1, c1), (r2, c2)) in neighbor_cells:
+        clauses.append((treeVar(r, c, r1, c1), treeVar(r, c, r2, c2)))
+    
+# Every tent matches at least one tree
+for r in range(grid.height):
+    for c in range(grid.width):
+        neighbor_cells = boundFilter([(r-1, c), (r+1, c), (r, c-1), (r, c+1)], grid.height, grid.width)
+        clause = [-tentVar(r, c)]
+        for (r_n, c_n) in neighbor_cells:
+            if (r_n, c_n) in grid.trees:
+                clause.append(treeVar(r, c, r_n, c_n))
+        clauses.append(tuple(clause))
+
+
