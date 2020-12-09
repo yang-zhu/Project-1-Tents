89d309889b14bfb2ed46bd843c89730622a53a18
YizeSun <Yize.Sun@campus.lmu.de>
2020-12-09 17:18:10 +0100

    clean import

--- gui.py
+++ gui.py
@@ -6,7 +6,7 @@ from PyQt5.QtCore import *
 from PyQt5.QtGui import *
 from PyQt5.QtWidgets import *
 
-sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__))))
+#sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__))))
 from encoding import solveGrid, Grid
 from display_solution import assetsPath
 
